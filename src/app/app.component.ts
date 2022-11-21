3af1a55455956ed0588f59342b7a0c5113d4dbd8
Omershlomo1 <omershlomo441@gmail.com>
2022-11-21 23:31:16 +0200

    Commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2Tl19xWN8skXzW6LcCWKHFjjkloImYW8",
+  authDomain: "omerprofile-dda24.firebaseapp.com",
+  projectId: "omerprofile-dda24",
+  storageBucket: "omerprofile-dda24.appspot.com",
+  messagingSenderId: "135308939563",
+  appId: "1:135308939563:web:5516b9f89070c4cde7b56e",
+  measurementId: "G-788RW2ZB4N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-root',
